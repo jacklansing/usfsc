5df147e1ccb0ef5fe307f68b63608b78163df794
Jack Lansing <jack.lansing@live.com>
2020-12-06 14:05:54 -0500

    update new skus for membership

--- lib/utils/getMembershipSKU.ts
+++ lib/utils/getMembershipSKU.ts
@@ -1,9 +1,13 @@
 const getMembershipSKU = (membershipType: string) => {
   switch (membershipType) {
     case 'individual':
-      return 'price_1HHxlPIKEd7Nl0FwtfvssiOg';
+      return 'price_0HvP0H3vwmRgFgMWbbU9gXWq';
     case 'family':
-      return 'price_1HqkxvIKEd7Nl0FwKCZBicTh';
+      return 'price_0HvP1G3vwmRgFgMWaTmU37S4';
+    case 'introductory':
+      return 'price_0HvP3n3vwmRgFgMWwc7vbXxX';
+    case 'professional':
+      return 'price_0HvP7l3vwmRgFgMWZQr5i9Q6';
     default:
       throw new Error('Membership type invalid');
   }
