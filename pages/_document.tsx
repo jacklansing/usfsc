de129aa9c4c77e6e2ea66d3ffb1de0d43a859515
Jack Lansing <jack.lansing@live.com>
2020-12-09 17:23:05 -0500

    ga

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -11,6 +11,20 @@ export default class extends Document {
     return (
       <Html lang="en">
         <Head>
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-CYJVES6QZF"
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-CYJVES6QZF');
+        `,
+            }}
+          />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
